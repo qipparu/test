31c0553f56c988853b9f4ad67685b582e2d2bdc2
Shapkin Maxim Vyacheslavovich <88982156+qipparu@users.noreply.github.com>
2024-04-06 22:58:45 +0500

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,16 +1,10 @@
 (function () {
-    'use strict';	
+    'use strict';
 
     Lampa.Listener.follow('app', (e) => {
         if (e.type == 'ready') {
-            setTimeout(function () {
-                // Создаем новый элемент для раздела аниме
-                var animeSection = document.createElement('div');
-                animeSection.textContent = 'Это раздел аниме';
-
-                // Вставляем новый элемент в DOM
-                document.body.appendChild(animeSection);
-            }, 10);
+            // Вместо удаления элемента, добавим содержимое в него или покажем его
+            $("[data-action=anime]").eq(0).show(); // Показать элемент с атрибутом data-action="anime"
         }
     });
 
